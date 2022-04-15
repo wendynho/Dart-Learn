f6441909c67109983570ea13a297ef468340e5ac
Wendel Benevides Matos Filho <98962827+wendynho@users.noreply.github.com>
2022-04-15 13:24:09 -0300

    Add files via upload

---
+++ declarando_variaveis.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "joesfabric";
+  int gears = 200;
+  double price = 5.50;
+  bool aindatem = true;
+
+  print("The name of the store is: $name");
+  print("There are $gears gears at the stock");
+  print("A $name costs $price dollars");
+}
