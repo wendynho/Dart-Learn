89737c1a9bd62b79b1c29b94dd14a5ae9948aebb
Wendel Benevides Matos Filho <98962827+wendynho@users.noreply.github.com>
2022-04-15 14:08:32 -0300

    Add files via upload

---
+++ operacoes_aritmeticas.dart
@@ -0,0 +1,15 @@
+void main() {
+  //declarando variáveis double.
+  double num1 = 10.0;
+  double num2 = 5.0;
+  //realizando operações
+  double result = num1 + num2;
+  print(result);
+
+  double num3 = 20.0;
+  double num4 = 2;
+  double result2 = num3 * num4;
+  print(result2);
+
+  print(result += result2);
+}
