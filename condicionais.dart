993e98ea119d0e894c0bb08c9addb25f89335f12
Wendel Benevides Matos Filho <98962827+wendynho@users.noreply.github.com>
2022-04-16 11:57:49 -0300

    Add files via upload

---
+++ condicionais.dart
@@ -0,0 +1,15 @@
+void main() {
+  //usando condicionais
+
+//O professor Leandro quer ter controle sobre as notas e situação escolar dos seus alunos.
+
+  double nota = 3.0;
+
+  if (nota >= 6) {
+    print("Sua nota é $nota, você está aprovado :)");
+  } else if (nota == 10.0) {
+    print("Sua nota é $nota, Parabéns!!! você está aprovadíssimo");
+  } else {
+    print("Sua nota é $nota, você está reprovado :(");
+  }
+}
