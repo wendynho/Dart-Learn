a386c245bdea9fc0f188a4e2c08bd3a7aed3d09f
Wendel Benevides Matos Filho <98962827+wendynho@users.noreply.github.com>
2022-05-01 11:59:59 -0300

    Add files via upload

---
+++ Loops.dart
@@ -0,0 +1,14 @@
+//loops em dart
+void main() {
+  int i = 0;
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
+  print('Terminou!');
+
+//resumindo com for
+  for (int i = 0; i <= 10; i++) {
+    print(i);
+  }
+}
