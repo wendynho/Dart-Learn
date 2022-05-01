6c96b36651d66d47170680462a4699071d4c35ba
Wendel Benevides Matos Filho <98962827+wendynho@users.noreply.github.com>
2022-05-01 12:27:35 -0300

    Add files via upload

---
+++ construtores.dart
@@ -0,0 +1,23 @@
+class Pessoa {
+  String? nome;
+  int? idade;
+  double? altura;
+
+//definindo construtores
+  Pessoa(this.nome, this.idade, this.altura);
+
+  void dancar() {
+    print("$nome está dançando!");
+  }
+}
+
+void main() {
+  Pessoa pessoa1 = new Pessoa("Liz Mell", 30, 1.80);
+
+  Pessoa pessoa2 = Pessoa("Vitinho", 28, 1.80);
+
+  print(pessoa1.nome);
+  print(pessoa2.nome);
+
+  pessoa1.dancar();
+}
